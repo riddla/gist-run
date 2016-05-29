9e1263919a425ed39393df15dd6fb4c54963beea
Volker Rose <volker.rose@gmail.com>
2016-05-29 19:43:57 +0200

    Add link to to gist on fallback page
    Implements https://github.com/gist-run/gist-run/issues/31.

--- src/ui/fallback.js
+++ src/ui/fallback.js
@@ -1,3 +1,16 @@
+import {inject} from 'aurelia-framework';
+import {QueryString} from '../editing/query-string';
+
+@inject(QueryString)
 export class UnsupportedBrowser {
-  
+  constructor(queryString) {
+    this.queryString = queryString;
+  }
+
+  activate() {
+    return this.queryString.read()
+      .then(gist => {
+        this.gistUrl = gist.html_url;
+      });
+  }
 }
